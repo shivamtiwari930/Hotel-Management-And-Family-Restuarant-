8fc323ed6e17611aa0030eba41b3a261f8c32e49
shivamtiwari930 <76580344+shivamtiwari930@users.noreply.github.com>
2021-07-08 10:31:34 +0530

    Initial commit

---
+++ nbproject/private/profiler/ResetResultsProfilingPoint.pp
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
+<properties>
+<entry key="0_p_name">Reset Results at NewJFrame7:301</entry>
+<entry key="1_p_enabled">true</entry>
+<entry key="1_p_location_file">{$projectDirectory}/test/NewJFrame7.java</entry>
+<entry key="0_p_location_file">{$projectDirectory}/test/NewJFrame7.java</entry>
+<entry key="1_p_location_offset">-2147483648</entry>
+<entry key="0_p_enabled">true</entry>
+<entry key="1_p_location_line">337</entry>
+<entry key="1_p_name">Reset Results at NewJFrame7:337</entry>
+<entry key="0_p_location_line">308</entry>
+<entry key="0_p_location_offset">-2147483648</entry>
+</properties>
---
+++ test/JavaApplicatio3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaApplicatio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        new NewJFrame().setVisible(true); // TODO code application logic here
+    }
+    
+}
